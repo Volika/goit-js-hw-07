dba55770c06a832a88a38b74ec01889b8fe44931
Valeriia <volikavs@gmail.com>
2022-06-01 09:22:03 +0300

    update 01

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,7 +1,9 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 const galleryImages = document.querySelector('.gallery')
-
+galleryImages.addEventListener("click", (event) => {
+    event.preventDefault();
+})
 const numberOfItems = galleryItems.length;
 console.log(numberOfItems);
 
@@ -9,17 +11,37 @@ createGallery(numberOfItems);
 
 function createGallery(numberOfItems) {
     const items = [];
-
     for (let i = 0; i < numberOfItems; i += 1) {
-        const item = document.createElement('img');
-        item.setAttribute('src', galleryItems[i].preview);
-        item.setAttribute('alt', galleryItems[i].description);
-        item.setAttribute('width', '400px');
-        item.setAttribute('height', '260px');
-        console.log(item);
+        const itemImg = createImgElement(galleryItems[i].preview, galleryItems[i].original, galleryItems[i].description);
+        const itemA = createAElement(galleryItems[i].original);
+        const itemDiv = createDivElement();
+        itemA.appendChild(itemImg);
+        itemDiv.appendChild(itemA);
+        let item = itemDiv;
+        console.log(item)
         items.push(item);
     }
     galleryImages.append(...items);
 }
 
+function createImgElement(preview, original, description) {
+    const item = document.createElement('img');
+    item.classList.add('gallery__image');
+    item.setAttribute('src', preview);
+    item.setAttribute('alt', description);
+    item.setAttribute('data-source', original);
+    return item;
+}
+function createAElement(original) {
+    const item = document.createElement('a');
+    item.classList.add('gallery__link');
+    item.setAttribute('href', original);
+    return item;
+}
+
+function createDivElement() {
+    const item = document.createElement('div');
+    item.classList.add('gallery__item');
+    return item;
+}
 
