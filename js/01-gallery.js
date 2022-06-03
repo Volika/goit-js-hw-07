d6d17a2696eb7bfdcf5ffa38a6fd08db428c5d31
Valeriia <volikavs@gmail.com>
2022-06-04 00:23:29 +0200

    summary

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -3,9 +3,18 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 const galleryImages = document.querySelector('.gallery');
 galleryImages.addEventListener("click", selectGalleryImages);
-const numberOfItems = galleryItems.length;
+// const numberOfItems = galleryItems.length;
 
-createGallery(numberOfItems);
+const imageEl = galleryItems.map(({preview, original, description}) => {
+    return `<div class="gallery__item"><a class="gallery__link" href='${original}'>
+    <img class="gallery__image" src ='${preview}' alt ='${description}' data-source='${original}'/>
+  </a>
+</div>`;
+}).join("");
+
+galleryImages.insertAdjacentHTML("afterbegin", imageEl);
+
+// createGallery(numberOfItems);
 
 function selectGalleryImages(event) {
     event.preventDefault();
@@ -24,44 +33,52 @@ function selectedImagesShow(images) {
     `);
     instance.show();
 
-    document.addEventListener("keydown", ({key}) => {if (key === 'Escape') {
+    document.addEventListener("keydown",
+        ({ key }) => {
+            if (key === 'Escape') {
         instance.close();
 }
-});
+    });
+    document.removeEventListener("keydown",
+        ({ key }) => {
+            if (key === 'Escape') {
+        instance.close();
+}
+    });
 }
 
-function createGallery(numberOfItems) {
-    const items = [];
-    for (let i = 0; i < numberOfItems; i += 1) {
-        const itemImg = createImgElement(galleryItems[i].preview, galleryItems[i].original, galleryItems[i].description);
-        const itemA = createAElement(galleryItems[i].original);
-        const itemDiv = createDivElement();
-        itemA.appendChild(itemImg);
-        itemDiv.appendChild(itemA);
+// function createGallery(numberOfItems) {
+//     const items = [];
+//     for (let i = 0; i < numberOfItems; i += 1) {
+//         const itemImg = createImgElement(galleryItems[i].preview, galleryItems[i].original, galleryItems[i].description);
+//         const itemA = createAElement(galleryItems[i].original);
+//         const itemDiv = createDivElement();
+//         itemA.appendChild(itemImg);
+//         itemDiv.appendChild(itemA);
 
-        items.push(itemDiv);
-    }
-    galleryImages.append(...items);
-}
+//         items.push(itemDiv);
+//     }
+//     galleryImages.append(...items);
+// }
 
-function createImgElement(preview, original, description) {
-    const item = document.createElement('img');
-    item.classList.add('gallery__image');
-    item.setAttribute('src', preview);
-    item.setAttribute('alt', description);
-    item.setAttribute('data-source', original);
-    return item;
-}
-function createAElement(original) {
-    const item = document.createElement('a');
-    item.classList.add('gallery__link');
-    item.setAttribute('href', original);
-    return item;
-}
+// function createImgElement(preview, original, description) {
+//     const item = document.createElement('img');
+//     item.classList.add('gallery__image');
+//     item.setAttribute('src', preview);
+//     item.setAttribute('alt', description);
+//     item.setAttribute('data-source', original);
+//     return item;
+// }
+// function createAElement(original) {
+//     const item = document.createElement('a');
+//     item.classList.add('gallery__link');
+//     item.setAttribute('href', original);
+//     return item;
+// }
 
-function createDivElement() {
-    const item = document.createElement('div');
-    item.classList.add('gallery__item');
-    return item;
-}
+// function createDivElement() {
+//     const item = document.createElement('div');
+//     item.classList.add('gallery__item');
+//     return item;
+// }
 
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -3,9 +3,18 @@ import { galleryItems } from './gallery-items.js';
 
 const galleryImages = document.querySelector('.gallery');
 galleryImages.addEventListener("click", selectGalleryImages);
-const numberOfItems = galleryItems.length;
 
-createGallery(numberOfItems);
+// const numberOfItems = galleryItems.length;
+// createGallery(numberOfItems);
+
+const imageEl = galleryItems.map(({preview, original, description}) => {
+    return `<a class="gallery__item" href='${original}'>
+    <img class="gallery__image" src ='${preview}' alt ='${description}'/>
+  </a>`;
+}).join("");
+
+galleryImages.insertAdjacentHTML("afterbegin", imageEl);
+
 
 function selectGalleryImages(event) {
     event.preventDefault();
@@ -16,40 +25,35 @@ function selectGalleryImages(event) {
    
     let lightbox = new SimpleLightbox('.gallery a', {
         captionsData: 'alt',
-        captionsDelay: 250,
+        captionsDelay: 1000,
         captionsPosition: 'bottom',
     });
     
 }
 
-function createGallery(numberOfItems) {
-    const items = [];
- 
-    for (let i = 0; i < numberOfItems; i += 1) {
-        const itemImg = createImgElement(galleryItems[i].preview, galleryItems[i].description);
-        const itemA = createAElement(galleryItems[i].original);
-      
-        itemA.appendChild(itemImg);
-
-        items.push(itemA);
-    }
-  
-    galleryImages.append(...items);
-}
-
-function createImgElement(preview, description) {
-    const item = document.createElement('img');
-    item.classList.add('gallery__image');
-    item.setAttribute('src', preview);
-    item.setAttribute('alt', description);
-    return item;
-}
-function createAElement(original) {
-    const item = document.createElement('a');
-    item.classList.add('gallery__item');
-    item.setAttribute('href', original);
-    item.setAttribute('data-lightbox', "lbox");
-    return item;
-}
+// function createGallery(numberOfItems) {
+//     const items = [];
+//     for (let i = 0; i < numberOfItems; i += 1) {
+//         const itemImg = createImgElement(galleryItems[i].preview, galleryItems[i].description);
+//         const itemA = createAElement(galleryItems[i].original);      
+//         itemA.appendChild(itemImg);
+//         items.push(itemA);
+//     }
+//     galleryImages.append(...items);
+// }
+// function createImgElement(preview, description) {
+//     const item = document.createElement('img');
+//     item.classList.add('gallery__image');
+//     item.setAttribute('src', preview);
+//     item.setAttribute('alt', description);
+//     return item;
+// }
+// function createAElement(original) {
+//     const item = document.createElement('a');
+//     item.classList.add('gallery__item');
+//     item.setAttribute('href', original);
+//     item.setAttribute('data-lightbox', "lbox");
+//     return item;
+// }
 
 
